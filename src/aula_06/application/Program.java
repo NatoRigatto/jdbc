77ae3a0519a0b018db6acc1bd4a0feb6141c6aba
Nato Rigatto <rigatto_88@hotmail.com>
2024-09-08 07:52:15 -0300

    Seller entity class

--- src/aula_06/application/Program.java
+++ src/aula_06/application/Program.java
@@ -1,10 +1,19 @@
 package aula_06.application;
 
+import java.util.Date;
+
+import aula_06.model.entities.Department;
+import aula_06.model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-
+		Department d = new Department(1, "Xablau");
+		
+		Seller s = new Seller(1, "Caga Tronco", "cagatronco@gmail.com", new Date(), 2000.0, d);
+		System.out.println(s);
+		
 	}
 
 }
